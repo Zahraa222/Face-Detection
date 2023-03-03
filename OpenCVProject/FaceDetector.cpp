1d0fca915a3e8eaaae84e9d057698a179b1b4418
zahraa222 <zzmmk2003@gmail.com>
2023-03-03 16:23:31 -0500

    Updated program, added Face detection using live webcam

--- OpenCVProject/FaceDetector.cpp
+++ OpenCVProject/FaceDetector.cpp
@@ -9,26 +9,79 @@ using namespace std;
 
 
 void main() {
-	string path = "Resources/Group.jpg"; //Face test image: Zahraa.jpg (me) or Group.jpg (Sample Group photo)
-	Mat img = imread(path);
-
-
-
+	string type;
+	Mat img;
 	CascadeClassifier faceCascade;
 	faceCascade.load("Resources/haarcascade_frontalface_default.xml"); //loads cascade to detect faces
-
-	if (faceCascade.empty()) { cout << "XML file not loaded" << endl; }
-
+	if (faceCascade.empty()) { cout << "XML file not loaded" << endl; } //outputs when XML is loaded or invalid
 	vector<Rect> faces; //to detect the face and store it, we need to store binding boxes(rectangles)
-	faceCascade.detectMultiScale(img, faces, 1.1, 10); //detects objects and returns as rectangles
 
-	for (int i = 0; i < faces.size(); i++) {
 
-		rectangle(img, faces[i].tl(), faces[i].br(), Scalar(0, 0, 255), 3); //draws rectangel from tl: top left of the face to br: bottom right of the face
+	//user input for facial detection type
+	cout << "Test image or live webcam? type 'image' or 'webcam': ";
+	cin >> type;
+
+	//Image Facial detection
+	if ((type == "image") || (type == "Image")){
+		string path = "Resources/Group.jpg"; //Face test image: Zahraa.jpg (me) or Group.jpg (Sample Group photo)
+		img = imread(path);
+		faceCascade.detectMultiScale(img, faces, 1.3, 5); //detects objects and returns as rectangles
+
+		for (int i = 0; i < faces.size(); i++) {
+
+			rectangle(img, faces[i].tl(), faces[i].br(), Scalar(0, 0, 255), 3); //draws rectangel from tl: top left of the face to br: bottom right of the face
+
+			//Top right corner, text output will display the number of faces visible
+			if (faces.size() == 1) {
+				putText(img, to_string(faces.size()) + " Face Found :)", Point(10, 40), FONT_HERSHEY_DUPLEX, 1, Scalar(0, 0, 0), 2);
+			}
+
+			else {
+				putText(img, to_string(faces.size()) + " Faces Found :)", Point(10, 40), FONT_HERSHEY_DUPLEX, 1, Scalar(0, 0, 0), 2);
+			}
+		}
+
+		imshow("Image Test", img);
+		waitKey(0);
 	}
 
-	imshow("Image Test", img);
-	waitKey(0);
+
+	//Live webcam Facial detection
+	if (type == "webcam" || type == ("Webcam")) {
+		VideoCapture video(0);
+
+		while (true) {
+			video.read(img);
+			faceCascade.detectMultiScale(img, faces, 1.3, 5); //detects objects and returns as rectangles
+
+			for (int i = 0; i < faces.size(); i++) {
+
+				//draws rectangle from tl: top left of the face to br: bottom right of the face, Rectangle color: red
+				rectangle(img, faces[i].tl(), faces[i].br(), Scalar(0, 0, 255), 3); 
+
+				//Top right corner, text output will display the number of faces visible
+				if (faces.size() == 1) {
+					putText(img, to_string(faces.size()) + " Face Found :)", Point(10, 40), FONT_HERSHEY_DUPLEX, 1, Scalar(0, 0, 0), 2);
+				}
+
+				else {
+					putText(img, to_string(faces.size()) + " Faces Found :)", Point(10, 40), FONT_HERSHEY_DUPLEX, 1, Scalar(0, 0, 0), 2);
+				}
+			
+			}
+
+			imshow("Live Detection", img);
+			waitKey(1);
 
 
+
+		}
+	}
+	else {
+		cout << "invlaid input. Please type 'image' or 'webcam'";
+		cin >> type;
+	}
+
+
+	system("PAUSE");
 }
